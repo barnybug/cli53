5a37145316d3429109a49830f39a3883abaa91e0
Nathan J. Mehl <n@weatherbill.com>
2011-08-31 22:41:55 -0700

    expand dump options to print either the zone xml or the changeset xml

--- src/cli53/cli53.py
+++ src/cli53/cli53.py
@@ -10,7 +10,7 @@ from cStringIO import StringIO
 from time import sleep
 
 # needs latest boto from github: http://github.com/boto/boto
-# git clone git://github.com/boto/boto 
+# git clone git://github.com/boto/boto
 try:
     import boto.route53, boto.jsonresponse, boto.exception
 except ImportError:
@@ -97,15 +97,15 @@ def pprint(obj, findent='', indent=''):
 def cmd_list(args):
     ret = r53.get_all_hosted_zones()
     pprint(ret.ListHostedZonesResponse)
-    
+
 def cmd_info(args):
     ret = r53.get_hosted_zone(args.zone)
     pprint(ret.GetHostedZoneResponse)
-    
+
 def text_element(parent, name, text):
     el = et.SubElement(parent, name)
     el.text = text
-    
+
 def is_root_soa_or_ns(name, rdataset):
     rt = dns.rdatatype.to_text(rdataset.rdtype)
     return (rt in ('SOA', 'NS') and name.to_text() == '@')
@@ -116,7 +116,7 @@ def paginate(iterable, size):
   while item:
     yield item
     item = list(itertools.islice(it, size))
-    
+
 class BindToR53Formatter(object):
     def _build_list(self, zone, exclude=None):
         li = []
@@ -125,7 +125,7 @@ class BindToR53Formatter(object):
                 if not exclude or not exclude(name, rdataset):
                     li.append((name, rdataset))
         return li
-    
+
     def create_all(self, zone, old_zone=None, exclude=None):
         creates = self._build_list(zone, exclude)
         deletes = []
@@ -133,32 +133,52 @@ class BindToR53Formatter(object):
             deletes = self._build_list(old_zone, exclude)
         return self._xml_changes(zone, creates=creates, deletes=deletes)
 
+    def dump_xml(self, zone, exclude=None):
+        resource_record_sets = et.Element('ResourceRecordSets',
+                xmlns=boto.route53.Route53Connection.XMLNameSpace)
+
+        for rrname in zone.keys():
+            for rdataset in zone[rrname].rdatasets:
+                resource_record_set = et.SubElement(resource_record_sets, 'ResourceRecordSet')
+                text_element(resource_record_set, 'Name', rrname.derelativize(zone.origin).to_text())
+                text_element(resource_record_set, 'Type', dns.rdatatype.to_text(rdataset.rdtype))
+                text_element(resource_record_set, 'TTL', str(rdataset.ttl))
+                resource_records = et.SubElement(resource_record_set, 'ResourceRecords')
+                for rdtype in rdataset.items:
+                    resource_record = et.SubElement(resource_records, 'ResourceRecord')
+                    text_element(resource_record, 'Value', rdtype.to_text(origin=zone.origin, relativize=False))
+
+        out = StringIO()
+        et.ElementTree(resource_record_sets).write(out)
+        return out.getvalue()
+
+
     def delete_all(self, zone, exclude=None):
         return self._xml_changes(zone, deletes=self._build_list(zone, exclude))
-        
+
     def create_record(self, zone, name, rdataset):
         return self._xml_changes(zone, creates=[(name,rdataset)])
-        
+
     def delete_record(self, zone, name, rdataset):
         return self._xml_changes(zone, deletes=[(name,rdataset)])
-        
+
     def replace_record(self, zone, name, rdataset_old, rdataset_new):
         return self._xml_changes(zone, creates=[(name,rdataset_new)], deletes=[(name,rdataset_old)])
-        
+
     def _xml_changes(self, zone, creates=[], deletes=[]):
         for page in paginate(self._iter_changes(creates, deletes), 100):
             yield self._batch_change(zone, page)
-    
+
     def _iter_changes(self, creates, deletes):
         for chg, rdatasets in (('DELETE', deletes), ('CREATE', creates)):
             for name, rdataset in rdatasets:
                 yield chg, name, rdataset
-        
+
     def _batch_change(self, zone, chgs):
         root = et.Element('ChangeResourceRecordSetsRequest', xmlns=boto.route53.Route53Connection.XMLNameSpace)
         change_batch = et.SubElement(root, 'ChangeBatch')
         changes = et.SubElement(change_batch, 'Changes')
-        
+
         for chg, name, rdataset in chgs:
             change = et.SubElement(changes, 'Change')
             text_element(change, 'Action', chg)
@@ -170,21 +190,21 @@ class BindToR53Formatter(object):
             for rdtype in rdataset.items:
                 rr = et.SubElement(rrs, 'ResourceRecord')
                 text_element(rr, 'Value', rdtype.to_text(origin=zone.origin, relativize=False))
-                    
+
         out = StringIO()
         et.ElementTree(root).write(out)
         return out.getvalue()
-        
+
 class R53ToBindFormatter(object):
     def get_all_rrsets(self, r53, ghz, zone):
         rrsets = r53.get_all_rrsets(zone, maxitems=10)
         return self.convert(ghz, rrsets)
-    
+
     def convert(self, info, rrsets, z=None):
         if not z:
             origin = info.HostedZone.Name
             z = dns.zone.Zone(dns.name.from_text(origin))
-        
+
         for rrset in rrsets:
             name = rrset.name
             if '\\052' in name:
@@ -196,9 +216,9 @@ class R53ToBindFormatter(object):
             rdataset = _create_rdataset(rtype, ttl, rrset.resource_records)
             node = z.get_node(name, create=True)
             node.replace_rdataset(rdataset)
-        
+
         return z
-    
+
 re_quoted = re.compile(r'^".*"$')
 re_backslash = re.compile(r'\\(.)')
 def unquote(v):
@@ -250,10 +270,10 @@ def _create_rdataset(rtype, ttl, values):
             raise ValueError, 'record type %s not handled' % rtype
         rdataset.items.append(rdtype)
     return rdataset
-    
+
 def cmd_xml(args):
     print 'This functionality is no longer available due to changes in the boto library.'
-    
+
 re_dos = re.compile('\r\n$')
 re_origin = re.compile(r'\$ORIGIN (\S+)')
 re_include = re.compile(r'\$INCLUDE (\S+)')
@@ -281,13 +301,20 @@ def cmd_import(args):
     origin = m.group(1)
 
     zone = dns.zone.from_text(text, origin=origin, check_origin=True)
+
+    if args.dump:
+        f = BindToR53Formatter()
+        xml = f.dump_xml(zone)
+        print xml
+        return
+
     old_zone = None
     if args.replace:
         old_zone = _get_records(args)
 
     f = BindToR53Formatter()
     for xml in f.create_all(zone, old_zone=old_zone, exclude=is_root_soa_or_ns):
-        if args.dump:
+        if args.dumpchg:
             print xml
         else:
             ret = r53.change_rrsets(args.zone, xml)
@@ -309,7 +336,7 @@ def Zone(zone):
         raise ArgumentTypeError, 'Zone %r is ambiguous (matches: %s), please specify ID' % (zone, ', '.join(hzs))
     else:
         raise ArgumentTypeError, 'Zone %r not found' % zone
-    
+
 def _get_records(args):
     info = r53.get_hosted_zone(args.zone)
     f = R53ToBindFormatter()
@@ -319,14 +346,14 @@ def cmd_export(args):
     zone = _get_records(args)
     print '$ORIGIN %s' % zone.origin.to_text()
     zone.to_file(sys.stdout, relativize=not args.full)
-    
+
 def cmd_create(args):
     ret = r53.create_hosted_zone(args.zone)
     if args.wait:
         wait_for_sync(ret)
     else:
         pprint(ret.CreateHostedZoneResponse)
-    
+
 def cmd_delete(args):
     ret = r53.delete_hosted_zone(args.zone)
     if hasattr(ret, 'ErrorResponse'):
@@ -345,7 +372,7 @@ def find_key_nonrecursive(adict, key):
         for k, v in d.iteritems():
             if isinstance(v, dict):
                 stack.append(v)
-    
+
 def wait_for_sync(obj):
     waiting = 1
     id = find_key_nonrecursive(obj, 'Id')
@@ -363,7 +390,7 @@ def wait_for_sync(obj):
             sleep(1)
     print "completed"
     pprint(ret.GetChangeResponse)
-    
+
 def cmd_rrcreate(args):
     zone = _get_records(args)
     name = dns.name.from_text(args.rr, zone.origin)
@@ -405,11 +432,11 @@ def cmd_rrdelete(args):
                 if args.type == dns.rdatatype.to_text(rds.rdtype) or not args.type:
                     rdataset = rds
                     break
-                    
+
             if not rdataset:
                 print 'Record not found: %s, type: %s' % (args.rr, args.type)
                 return
-                
+
             print 'Deleting %s %s...' % (args.rr, dns.rdatatype.to_text(rds.rdtype))
 
             f = BindToR53Formatter()
@@ -422,7 +449,7 @@ def cmd_rrdelete(args):
                     pprint(ret.ChangeResourceRecordSetsResponse)
     else:
         print 'Record not found: %s' % args.rr
-    
+
 def cmd_rrpurge(args):
     zone = _get_records(args)
     f = BindToR53Formatter()
@@ -432,7 +459,7 @@ def cmd_rrpurge(args):
             wait_for_sync(ret)
         else:
             pprint(ret.ChangeResourceRecordSetsResponse)
-    
+
 def cmd_rrlist(args):
     zone = _get_records(args)
     print '\t'.join(["host","ttl","cls","type","data"])
@@ -443,43 +470,44 @@ def main():
     connection = boto.route53.Route53Connection()
     parser = argparse.ArgumentParser(description='route53 command line tool')
     subparsers = parser.add_subparsers(help='sub-command help')
-    
+
     supported_rtypes = ('A', 'AAAA', 'CNAME', 'SOA', 'NS', 'MX', 'PTR', 'SPF', 'SRV', 'TXT')
-    
+
     parser_list = subparsers.add_parser('list', help='list hosted zones')
     parser_list.set_defaults(func=cmd_list)
-    
+
     parser_list = subparsers.add_parser('info', help='get details of a hosted zone')
     parser_list.add_argument('zone', type=Zone, help='zone name')
     parser_list.set_defaults(func=cmd_info)
-    
+
     parser_describe = subparsers.add_parser('xml')
     parser_describe.add_argument('zone', type=Zone, help='zone name')
     parser_describe.set_defaults(func=cmd_xml)
-    
+
     parser_describe = subparsers.add_parser('export', help='export dns in bind format')
     parser_describe.add_argument('zone', type=Zone, help='zone name')
     parser_describe.add_argument('--full', action='store_true', help='export prefixes as full names')
     parser_describe.set_defaults(func=cmd_export)
-    
+
     parser_import = subparsers.add_parser('import', help='import dns in bind format')
     parser_import.add_argument('zone', type=Zone, help='zone name')
     parser_import.add_argument('-r', '--replace', action='store_true', help='replace all existing records (use with care!)')
     parser_import.add_argument('-f', '--file', type=argparse.FileType('r'), help='bind file')
     parser_import.add_argument('--wait', action='store_true', default=False, help='wait for changes to become live before exiting (default: false)')
-    parser_import.add_argument('--dump', action='store_true', help='dump xml to stdout')
+    parser_import.add_argument('--dump', action='store_true', help='dump zone file in xml format to stdout')
+    parser_import.add_argument('--dumpchg', action='store_true', help='dump xml change request to stdout')
     parser_import.set_defaults(func=cmd_import)
-    
+
     parser_create = subparsers.add_parser('create', help='create a hosted zone')
     parser_create.add_argument('zone', help='zone name')
     parser_create.add_argument('--wait', action='store_true', default=False, help='wait for changes to become live before exiting (default: false)')
     parser_create.set_defaults(func=cmd_create)
-    
+
     parser_delete = subparsers.add_parser('delete', help='delete a hosted zone')
     parser_delete.add_argument('zone', type=Zone, help='zone name')
     parser_delete.add_argument('--wait', action='store_true', default=False, help='wait for changes to become live before exiting (default: false)')
     parser_delete.set_defaults(func=cmd_delete)
-    
+
     parser_rrcreate = subparsers.add_parser('rrcreate', help='create a resource record')
     parser_rrcreate.add_argument('zone', type=Zone, help='zone name')
     parser_rrcreate.add_argument('rr', help='resource record')
@@ -489,26 +517,26 @@ def main():
     parser_rrcreate.add_argument('-r', '--replace', action='store_true', help='replace any existing record')
     parser_rrcreate.add_argument('--wait', action='store_true', default=False, help='wait for changes to become live before exiting (default: false)')
     parser_rrcreate.set_defaults(func=cmd_rrcreate)
-    
+
     parser_rrdelete = subparsers.add_parser('rrdelete', help='delete a resource record')
     parser_rrdelete.add_argument('zone', type=Zone, help='zone name')
     parser_rrdelete.add_argument('rr', help='resource record')
     parser_rrdelete.add_argument('type', nargs='?', choices=supported_rtypes, help='resource record type')
     parser_rrdelete.add_argument('--wait', action='store_true', default=False, help='wait for changes to become live before exiting (default: false)')
     parser_rrdelete.set_defaults(func=cmd_rrdelete)
-    
+
     parser_rrpurge = subparsers.add_parser('rrpurge', help='purge all resource records')
     parser_rrpurge.add_argument('zone', type=Zone, help='zone name')
     parser_rrpurge.add_argument('--confirm', required=True, action='store_true', help='confirm you definitely want to do this!')
     parser_rrpurge.add_argument('--wait', action='store_true', default=False, help='wait for changes to become live before exiting (default: false)')
     parser_rrpurge.set_defaults(func=cmd_rrpurge)
-    
+
     parser_rrlist = subparsers.add_parser('rrlist', help='list all resource records')
     parser_rrlist.add_argument('zone', type=Zone, help='zone name')
     parser_rrlist.set_defaults(func=cmd_rrlist)
 
     args = parser.parse_args()
     args.func(args)
-    
+
 if __name__=='__main__':
     main()
