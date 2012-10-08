e81d5778ed5f81d5d479bf828de1a854c24af75e
Barnaby Gray <barnaby@pickle.me.uk>
2012-10-08 16:33:07 +0100

    Fix tests on python 2.6

--- tests/test_domains.py
+++ tests/test_domains.py
@@ -2,10 +2,24 @@ import unittest
 import subprocess
 import random
 
+# copied from python 2.7 for python 2.6
+def check_output(*popenargs, **kwargs):
+    if 'stdout' in kwargs:
+        raise ValueError('stdout argument not allowed, it will be overridden.')
+    process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+    output, unused_err = process.communicate()
+    retcode = process.poll()
+    if retcode:
+        cmd = kwargs.get("args")
+        if cmd is None:
+            cmd = popenargs[0]
+        raise subprocess.CalledProcessError(retcode, cmd, output=output)
+    return output
+
 class DomainsTest(unittest.TestCase):
     def _cmd(self, cmd, *args):
         pargs = ('scripts/cli53', cmd) + args
-        return subprocess.check_output(pargs, stderr=subprocess.STDOUT)
+        return check_output(pargs, stderr=subprocess.STDOUT)
         
     def _unique_name(self):
         return 'temp%d.com' % random.randint(0, 65535)
