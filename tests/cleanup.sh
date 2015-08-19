12f888d583ca6693a240dc6b797b367b7d9128d7
Barnaby Gray <barnaby@pickle.me.uk>
2015-08-19 15:15:43 +0100

    Fixes to tests

--- tests/cleanup.sh
+++ tests/cleanup.sh
@@ -4,5 +4,5 @@ export PYTHONPATH=.
 COMMENT="unittests${TRAVIS_JOB_ID}"
 # Clean up script to run after the tests to ensure any temporary
 # domains are deleted to avoid racking up ec2 costs.
-scripts/cli53 list | grep -A2 "$COMMENT" | perl -ne 'print "$1\n" if m!Id: /hostedzone/(.+)!' | xargs -n 1 scripts/cli53 rrpurge --confirm
-scripts/cli53 list | grep -A2 "$COMMENT" | perl -ne 'print "$1\n" if m!Id: /hostedzone/(.+)!' | xargs -n 1 scripts/cli53 delete
+scripts/cli53 list | grep -A2 "$COMMENT" | perl -ne 'print "$1\n" if m!Id: /hostedzone/(.+)!' | xargs -n 1 -r scripts/cli53 rrpurge --confirm
+scripts/cli53 list | grep -A2 "$COMMENT" | perl -ne 'print "$1\n" if m!Id: /hostedzone/(.+)!' | xargs -n 1 -r scripts/cli53 delete
