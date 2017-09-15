d8f8b00fa7e98a29dd4f377017751e37be2d91c2
Lukáš Lalinský <lukas@oxygene.sk>
2017-09-15 15:51:29 +0200

    Patch github.com/miekg/dns

--- vendor/github.com/miekg/dns/privaterr.go
+++ vendor/github.com/miekg/dns/privaterr.go
@@ -129,7 +129,7 @@ func PrivateHandle(rtypestr string, rtype uint16, generator func() PrivateRdata)
 			return nil, &ParseError{f, err.Error(), l}, ""
 		}
 
-		return rr, nil, ""
+		return rr, nil, l.comment
 	}
 
 	typeToparserFunc[rtype] = parserFunc{setPrivateRR, true}
